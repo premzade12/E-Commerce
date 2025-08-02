675131d4a326ec55b2524a79d08ce7d4677859e7
prem <zadeprem1234@gmail.com>
2025-08-02 20:03:24 +0530

    E-Commerce

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+let port = 8000;
+
+let app = express();
+
+app.get('/',(req,res)=>{
+    res.send("Hello From Server")
+})
+
+app.listen(port, ()=>{
+    console.log("Hello From Server");
+})
+
