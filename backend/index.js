8e41117aa926b17401334f221e5731f5865a26d5
hyde-51 <ayush131gajbhiye@gmail.com>
2025-08-02 21:37:04 +0530

    initial

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,10 @@ let port = 8000;
 let app = express();
 
 app.get('/',(req,res)=>{
-    res.send("Hello From Server")
+    res.send("Hello From world")
 })
 
 app.listen(port, ()=>{
-    console.log("Hello From Server");
+    console.log("Hello From Srver");
 })
 
