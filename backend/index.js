42abf8246a82a628d826eecb285e96eb757cd212
hyde-51 <ayush131gajbhiye@gmail.com>
2025-08-03 00:48:39 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDb from "./config/db.js";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoutes.js";
+import cors from 'cors'
 dotenv.config();
 
 let port = process.env.PORT || 6000
@@ -12,6 +13,10 @@ let app = express();
 //middleware
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}));
 
 app.use("/api/auth",authRoutes);
 
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='text-[30px] text-red-400'>
+      home page
+    </div>
+  )
+}
+
+export default Home
