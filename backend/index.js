fdb6985abc2df0e3f8b34b40b3b59617c102e953
prem <zadeprem1234@gmail.com>
2025-08-02 23:14:44 +0530

    backend init

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,23 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/authRoutes.js";
+dotenv.config();
 
-let port = 8000;
+let port = process.env.PORT || 6000
 
 let app = express();
 
-app.get('/',(req,res)=>{
-    res.send("Hello From world")
-})
+//middleware
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth",authRoutes);
+
 
 app.listen(port, ()=>{
-    console.log("Hello From world");
+    console.log("Hello From Server");
+    connectDb();
 })
 
---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { login, logOut, registration } from "../controller/authController.js";
+
+const authRoutes = express.Router();
+
+authRoutes.post("/registration",registration);
+authRoutes.post("/login",login);
+authRoutes.get("/logOut",logOut);
+
+export default authRoutes;
