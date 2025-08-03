9c723262370a887bf70905c24c6fc6b04f7cf7d9
prem <zadeprem1234@gmail.com>
2025-08-03 22:56:04 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -13,12 +13,19 @@ let app = express();
 //middleware
 app.use(express.json());
 app.use(cookieParser());
+app.use((req, res, next) => {
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  next();
+});
 app.use(cors({
     origin:"http://localhost:5173",
     credentials:true
 }));
 
+
 app.use("/api/auth",authRoutes);
+
 
 
 app.listen(port, ()=>{
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { login, logOut, registration } from "../controller/authController.js";
+import { googleLogin, login, logOut, registration } from "../controller/authController.js";
 
 const authRoutes = express.Router();
 
 authRoutes.post("/registration",registration);
 authRoutes.post("/login",login);
 authRoutes.get("/logOut",logOut);
+authRoutes.post("/googlelogin",googleLogin);
 
 export default authRoutes;
