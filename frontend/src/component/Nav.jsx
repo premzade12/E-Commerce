2f51c20f5b482127f38a8dacc2258d57cd56e684
prem <zadeprem1234@gmail.com>
2025-08-07 22:23:35 +0530

    update

---
+++ frontend/src/component/Nav.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div className='w-[100vw] h-[70px] bg-[#ecfafaec] z-10 fixed top-0 flex items-center justify-between
+    px-[30px] shadow-md'>Nav</div>
+  )
+}
+
+export default Nav
