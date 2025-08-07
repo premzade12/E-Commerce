865d622874ff0277dc35e5b37f83fcac135da946
hyde-51 <ayush131gajbhiye@gmail.com>
2025-08-07 22:34:26 +0530

    update

---
+++ frontend/src/component/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = ({heroData,heroCount,setHeroCount}) => {
+  return (
+    <div className='w-[40%] h-[100%] relative' >
+      <div className=''></div>
+    </div>
+  )
+}
+
+export default Hero
