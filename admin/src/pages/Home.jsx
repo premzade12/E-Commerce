07476c2c5630a8a04131afeb8891b19d5365b0b4
hyde-51 <ayush131gajbhiye@gmail.com>
2025-08-08 23:14:08 +0530

    update

--- admin/src/pages/Home.jsx
+++ admin/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Nav from '../component/Nav'
 
 function Home() {
   return (
-    <div className='text-[30px]'>
-      df
+   <div className="w-screen h-screen bg-gradient-to-l from-[#141414] to-[#0c2025] text-white relative">
+      <Nav/>
+      
     </div>
   )
 }
