486a71ce89e68049f7a5604405cce745349515ce
hyde-51 <ayush131gajbhiye@gmail.com>
2025-08-10 00:02:33 +0530

    update

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Nav from '../component/Nav'
+import Sidebar from '../component/Sidebar'
 
 function Add() {
   return (
-    <div>
-      sd
+    <div className='w-screen h-screen bg-gradient-to-l from-[#141414] to-[#0c2025] text-white overflow-x-hidden relative'>
+      <Nav/>
+      <Sidebar/>
     </div>
   )
 }
