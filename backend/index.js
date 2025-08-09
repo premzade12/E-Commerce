1abc5737a38f52d0067f2d3dd46b09de480b6dd6
hyde-51 <ayush131gajbhiye@gmail.com>
2025-08-10 00:02:33 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoutes.js";
 import cors from 'cors'
 import userRoutes from "./routes/userRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
 dotenv.config();
 
 let port = process.env.PORT || 6000
@@ -27,7 +28,7 @@ app.use(cors({
 
 app.use("/api/auth",authRoutes);
 app.use("/api/user",userRoutes);
-
+app.use("/api/product",productRoutes)
 
 
 app.listen(port, ()=>{
