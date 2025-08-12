99b5b72062a223273ce9d92d383b6f5c995ad717
hyde-51 <ayush131gajbhiye@gmail.com>
2025-08-12 16:37:10 +0530

    update

--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -58,10 +58,37 @@ function ProductDetails() {
                     <img src={image} alt="" className='w-[100%] lg:h-[100%] text-[30px] text-white text-center rounded-md object-fill'/>
                 </div>
         </div>
-        <div className='lg:w-[50vw] w-[100vw] lg:h-[75vh] h-[40vh] lg:mt-[80px] flex items-center justify-start flex-col py-[20px] px-[30px] md:pb-[20px] md:pl-[20px] lg:pl-[0px] lg:px-[0px] lg:py-[0px] gap-[10px]'>
+        <div className='lg:w-[50vw] w-[100vw] lg:h-[75vh] h-[40vh] lg:mt-[80px] flex items-start justify-start flex-col py-[20px] px-[30px] md:pb-[20px] md:pl-[20px] lg:pl-[0px] lg:px-[0px] lg:py-[0px] gap-[10px]'>
             <h1 className='text-[40px] font-semibold text-[aliceblue]'>
-                {}
+                {productData.name.toUpperCase()}
             </h1>
+            <div className='flex items-center gap-1'>
+                <FaStar className='text-[20px] fill-[#FFD700]'/>
+                <FaStar className='text-[20px] fill-[#FFD700]'/>
+                <FaStar className='text-[20px] fill-[#FFD700]'/>
+                <FaStar className='text-[20px] fill-[#FFD700]'/>
+                <FaStarHalfAlt className='text-[20px] fill-[#FFD700]'/>
+                <p className='text-[18px] font-semibold pl-[5px] text-white'>
+                    (124)
+                </p>
+            </div>
+            <p className='text-[30px] font-semibold pl-[5px] text-white'>{currency}{productData.price}</p>
+            <p className='w-[80%] md:w-[60%] text-[20px] font-semibold pl-[5px] text-white'>{productData.description} and stylish, breathable cotton shirt with a modern slim fit. Easy to wash, super comfortable, and designed for effortless style.</p>
+            <div className='flex flex-col gap-[10px] my-[10px] '>
+                <p className='text-[25px] font-semibold pl-[5px] text-white'>
+                    Select Size
+                </p>
+                <div className='flex gap-2'>
+                    {
+                        productData.sizes.map((item,index)=>(
+                            <button key={index} className={`border py-2 px-4 bg-slate-300 rounded-md ${item === size ? 'bg-black text-[#2f97f1] text-[20px]' : ''}`} onClick={()=>setSize(item)}>
+                                {item}
+                            </button>
+                        ))
+                    }
+                </div>
+
+            </div>
         </div>
       </div>
     </div>
