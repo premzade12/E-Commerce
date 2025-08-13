5dd94dc0245ea071a804b14cbd2778662ebb08d7
prem <zadeprem1234@gmail.com>
2025-08-13 19:32:45 +0530

    update

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Nav from '../component/Nav'
+import Sidebar from '../component/Sidebar'
 
 function Orders() {
   return (
-    <div>
-      
+    <div className='w-[100vw] min-h-[100vh] bg-gradient-to-l from-[#141414] to-[#0c2025] text-[white]'>
+      <Nav/>
+      <div className='w-[100%] h-[100%] flex items-center lg:justify-start justify-center'>
+        <Sidebar/> 
+      </div>
     </div>
   )
 }
