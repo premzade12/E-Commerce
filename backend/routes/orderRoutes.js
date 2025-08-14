71711d1ec2f959c4993feecdbf2b702126e56b2f
hyde-51 <ayush131gajbhiye@gmail.com>
2025-08-14 12:27:38 +0530

    update

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import isAuth from '../middleware/isAuth.js'
-import { placeOrder, placeOrderStripe } from '../controller/orderController.js'
+import { allOrders, placeOrder, placeOrderStripe, updateStatus } from '../controller/orderController.js'
 import { createStripePayment,userOrders } from '../controller/orderController.js'
+import adminAuth from '../middleware/adminAuth.js'
+import { adminLogin } from '../controller/authController.js'
 
 
 const orderRoutes = express.Router()
@@ -11,5 +13,7 @@ orderRoutes.post("/placeorder",isAuth,placeOrder)
 orderRoutes.post("/create-stripe-payment", isAuth, createStripePayment);
 orderRoutes.post("/verifyStripe", isAuth, placeOrderStripe);
 orderRoutes.post("/userOrders", isAuth, userOrders)
+orderRoutes.post("/list", adminAuth, allOrders)
+orderRoutes.post("/status", adminAuth, updateStatus)
 
 export default orderRoutes
