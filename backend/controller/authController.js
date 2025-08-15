b96ec4b53e2831f8bc10b9861e2bb4e9d4a252fa
Prem Zade <143734541+premzade12@users.noreply.github.com>
2025-08-15 17:11:32 +0530

    Update authController.js

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -27,7 +27,7 @@ export const registration = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "none",
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -54,7 +54,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "none",
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -88,7 +88,7 @@ export const googleLogin = async(req,res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "none",
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -109,7 +109,7 @@ export const adminLogin = async (req,res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "none",
+      sameSite: "None",
       maxAge: 1 * 24 * 60 * 60 * 1000,
     });
 
